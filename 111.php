51c1bea01cfb73c62ea39aa584701c15772539e0
zhazha-yu <3038004529@qq.com>
2020-03-03 21:56:20 +0800

    update

--- 111.php
+++ 111.php
@@ -1,6 +1,10 @@
 <?php
-$x=5;
-$y=6;
-$z=$x+$y;
-echo $z;
+$txt1="Learn PHP";
+$txt2="W3School.com.cn";
+$cars=array("Volvo","BMW","SAAB");
+
+echo $txt1;
+echo "<br>";
+echo "Study PHP at $txt2";
+echo "My car is a {$cars[0]}";
 ?>
